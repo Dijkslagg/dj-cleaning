d68943585d0f4569fac669b44e6180da2d40004b
Koen <dijkslag.koen@gmail.com>
2024-12-11 03:54:46 +0100

    (fix) dbname change, oops

--- sql/cleaning.sql
+++ sql/cleaning.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS `s1450018_QBCORE.cleaning_jobs` (
+CREATE TABLE IF NOT EXISTS `cleaning_jobs` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `citizenid` varchar(50) NOT NULL,
     `job_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS `s1450018_QBCORE.cleaning_jobs` (
     PRIMARY KEY (`id`)
 );
 
-CREATE TABLE IF NOT EXISTS `s1450018_QBCORE.cleaning_levels` (
+CREATE TABLE IF NOT EXISTS `cleaning_levels` (
     `citizenid` varchar(50) NOT NULL,
     `level` int(11) NOT NULL DEFAULT 1,
     `experience` int(11) NOT NULL DEFAULT 0,
